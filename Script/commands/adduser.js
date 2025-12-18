73cd4a0cf2a6718af67f36b680509b1845b46b9e
MD. RAKIBUL ISLAM <120728216+rakibulislam6666@users.noreply.github.com>
2025-12-19 00:07:05 +0600

    Update adduser.js

--- Script/commands/adduser.js
+++ Script/commands/adduser.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adduser",
 	version: "2.4.3",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 ",
 	description: "Add user to the group by link or id",
 	commandCategory: "group",
 	usages: "[args]",
