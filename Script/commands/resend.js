3f2d4133e8935f2aea9a7b248c453af3ce7e983f
MD. RAKIBUL ISLAM <120728216+rakibulislam6666@users.noreply.github.com>
2025-12-19 01:05:14 +0600

    Update resend.js

--- Script/commands/resend.js
+++ Script/commands/resend.js
@@ -45,7 +45,7 @@ module.exports.handleEvent = async function ({ event, api, Users }) {
 
  if (!msg.attachment || msg.attachment.length === 0) {
  return api.sendMessage(
- `═════════════════════\n ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭\n═════════════════════\n\nকই গো সবাই দেখুন🥺\n@${userName} এই লুচ্ছায়\nমাত্র 👉 [${msg.msgBody}] 👈\nএই টেক্সট টা  রিমুভ দিছে😁\n\n═════════════════════\n 𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 𝗦𝗵𝗮𝗵𝗮𝗱𝗮𝘁 𝗦𝗔𝗛𝗨\n═════════════════════`,
+ `কই গো সবাই দেখুন🥺\n@${userName} এই লুচ্ছায়\nমাত্র 👉 [${msg.msgBody}] 👈\nএই টেক্সট টা  রিমুভ দিছে😁`,
  threadID,
  (err, info) => {
  if (!err && info) {
@@ -68,7 +68,7 @@ module.exports.handleEvent = async function ({ event, api, Users }) {
  }
 
  const resendMsg = {
- body: `@${userName} এই হালায় এই মাত্র এইডা রিমুভ দিছে🙆 সবাই দেখে নেও🐸😁${msg.msgBody ? `\n\nContent: ${msg.msgBody}` : ""}`,
+ body: `@${userName} এই হালায় এই মাত্র এইডা রিমুভ দিছে🙆 সবাই দেখে নেও😁${msg.msgBody ? `\n\nContent: ${msg.msgBody}` : ""}`,
  attachment: attachmentsList,
  mentions: [{ tag: userName, id: senderID }]
  };
